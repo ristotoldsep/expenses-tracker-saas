64fcd1280262e0fcb402b07194a368fa34d8b754
Risto Tõldsep <r.toldsep@gmail.com>
2025-01-30 00:43:48 +0200

    Improvements

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,4 +1,4 @@
-import "server-only";
+import "server-only"; // This file is only used in the server build
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
