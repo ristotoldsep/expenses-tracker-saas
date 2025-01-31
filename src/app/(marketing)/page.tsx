4b163109be8151ef207bcebfe7fe7f5a37a663c4
Risto Tõldsep <r.toldsep@gmail.com>
2025-01-31 12:46:57 +0200

    Made improvements

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -34,7 +34,7 @@ export default async function Home() {
       <FrontHeader isLoggedIn={isLoggedIn} isPayingMember={isPayingMember} />
       <div className="min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10 text-white py-[100px] relative">
       <div className="absolute -z-1 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-        <div className="max-w-[1300px] mx-auto flex flex-col xl:flex-row items-center justify-between gap-2 lg:gap-10 px-5">
+        <div className="max-w-[1300px] mx-auto flex flex-col-reverse xl:flex-row items-center justify-between gap-10 lg:gap-10 px-5">
           <Image
             src="/rahavoog_cover.webp"
             alt="Expenses Tracker app preview"
